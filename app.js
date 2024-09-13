84efe8e9986eacd52dc1ada0717bd2957019c22d
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-09-14 00:44:23 +0545

    backend setup done

--- app.js
+++ app.js
@@ -1,38 +1,67 @@
 const express = require("express");
 const socket = require("socket.io");
 const http = require("http");
-const {Chess} = require("chess.js");
+const { Chess } = require("chess.js");
 const path = require("path");
+const { log } = require("console");
 
 const app = express();
 const server = http.createServer(app);
 
-const io =  socket(server);
+const io = socket(server);
 
-const chess=new Chess();
+const chess = new Chess();
 
-let players={}; 
-let currentPlayer="w";
+let players = {};
+let currentPlayer = "w";
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-    res.render("index",{title:"Chess Game"});
-}); 
+  res.render("index", { title: "Chess Game" });
+});
 
 io.on("connection", (socket) => {
-    console.log("socket connection established");
-    // socket.on("join", (data) => {
-    //     players[socket.id] = data;
-    //     socket.join("room1");
-    //     if (Object.keys(players).length === 2) {
-    //         io.to("room1").emit("start", players);
-    //     }
-    // });
-})
+  console.log("socket connection established");
+  if (!players.white) {
+    players.white = socket.id;
+    socket.emit("playerRole", "w");
+  } else if (!players.black) {
+    players.black = socket.id;
+    socket.emit("playerRole", "b");
+  } else {
+    socket.emit("spectatorRole");
+  }
+  socket.on("disconnect", () => {
+    if (socket.id === players.white) {
+      delete players.white;
+    } else if (socket.id === players.black) {
+      delete players.black;
+    }
+  });
 
+  socket.on("move", (move) => {
+    try {
+      //valiate players of its his/her trun or not
+      if (chess.turn === "w" && socket.id !== players.white) return;
+      if (chess.turn === "b" && socket.id !== players.black) return;
+      const result = chess.move(move);
+      if (result) {
+        currentPlayer = chess.turn();
+        io.emit("move", move);
+        io.emit("boardState", chess.fen());
+      } else {
+        console.log("Invalid move : ",move);
+        socket.emit("invalidMove",move);
+    }
+} catch (err) {
+    console.log(err)
+    socket.emit("invalidMove",move);
+    }
+  });
+});
 
-server.listen(3000,()=>{
-    console.log("listening on port 3000");
+server.listen(3000, () => {
+  console.log("listening on port 3000");
 });
